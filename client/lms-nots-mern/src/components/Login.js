17bfd982b57cbe0851141e610423cd56befa0ceb
DiegoMurielG <yinga100@gmail.com>
2024-09-27 03:23:59 -0600

    Updated Login component

--- client/lms-nots-mern/src/components/Login.js
+++ client/lms-nots-mern/src/components/Login.js
@@ -12,7 +12,13 @@ export default function Login() {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post("https://lms-facultad-de-quimica.onrender.com/api/iniciar-sesion", { email, password })
+      .post(
+        "https://lms-facultad-de-quimica.onrender.com/api/iniciar-sesion",
+        { email, password },
+        {
+          withCredentials: true,
+        }
+      )
       .then((response) => {
         setWarning(response.data.message);
         // console.log(response.data);
